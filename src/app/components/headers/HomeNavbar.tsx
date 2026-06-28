90d932cf310235b008f99e5911b1247b34d16c4b
Sanjarbek9706 <san19970618@gmail.com>
2026-06-28 20:36:04 +0900

    fix: homeNavbar menu part refactoring

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -2,38 +2,27 @@ import { Box, Button, Container, Stack } from "@mui/material";
 import { NavLink } from "react-router-dom";
 
 export function HomeNavbar() {
-    const authMember = null;
+    const authMember = true;
 
     return (
         <div className="home-navbar">
-            <Container sx={{mt: "55px", height: "642px" }}>
-                <Stack 
-                sx={{height: "55px"}} 
-                flexDirection={"row"} 
-                justifyContent={"space-between"}
-                alignItems={"center"}
-                >
+            <Container className="navbar-container">
+                <Stack className="menu">
                     <Box>
                         <NavLink to="/">
-                         <img 
-                         style={{width: "125px", height: "30px" }} 
+                         <img className="brand-logo"
                          src="/icons/burak.svg"/>
                         </NavLink>
                     </Box>
-                    <Stack 
-                    flexDirection={"row"} 
-                    justifyContent={"space-between"}
-                    minWidth={"700px"}
-                    alignItems={"center"}
-                    >
-                    <Box className={"hover-line"}>
+                    <Stack className="links">
+                     <Box className={"hover-line"}>
                         <NavLink to="/" activeClassName={"underline"}>Home</NavLink>
-                    </Box>
+                     </Box>
                     <Box className={"hover-line"}>
                         <NavLink to="/products" activeClassName={"underline"}>Products</NavLink>
                     </Box >
                     {authMember ? (<Box className={"hover-line"}>
-                        <NavLink to="/orders" activeClassName={"underline"}>orders</NavLink>
+                        <NavLink to="/orders" activeClassName={"underline"}>Orders</NavLink>
                     </Box>
                    ) : null}
                     {authMember ? (<Box className={"hover-line"}>
@@ -47,7 +36,13 @@ export function HomeNavbar() {
 
                     {!authMember ? (<Box><Button 
                     variant="contained" 
-                    style={{background: "#3776CC", color: "#f8f8ff"}}>Login</Button></Box>) : (<img />)}
+                    className="login-button">Login</Button></Box>
+                   ) : (
+                    <img className="user-avatar"
+                        src={"/icons/default-user.svg"}
+                        aria-haspopup={"true"}
+                    />
+                    )}
                     </Stack>
                 </Stack>
                 <Stack>DETAIL</Stack>  
