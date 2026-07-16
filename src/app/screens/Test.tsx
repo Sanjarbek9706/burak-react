d1796aeca15866b6f30ac965148a07673a065b4b
Sanjarbek9706 <san19970618@gmail.com>
2026-07-16 20:28:44 +0900

    feat: create  Test class component

---
+++ src/app/screens/Test.tsx
@@ -0,0 +1,54 @@
+// @ts-nocheck
+
+import React, { Component } from "react";
+
+class Test extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1964
+    };
+  }
+
+  changeDetail = () => {
+    this.setState({
+        color: "blue", 
+        brand: "Tesla", 
+        model: "Model S", 
+        year: 2026,
+    });
+  };
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    //  runs after first reender => RETRIEVE DATA FROM BACKEND SERVER     
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+    // runs before companent unmount
+  }
+
+  componentDidUpdate() {}
+
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          Coler: {this.state.color} - Model: {this.state.model} from {" "}
+          {this.state.year}.
+        </p>
+        <button
+          type="button"
+          onClick={this.changeDetail}
+        >Change Detail </button>
+      </div>
+    );
+  }
+}
+
+export  default Test;
