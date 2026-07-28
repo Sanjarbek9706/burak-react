56c22638c3b610d4e7571bb0d60bef6e2795a5e6
Sanjarbek9706 <san19970618@gmail.com>
2026-07-29 03:21:57 +0900

    fix: create login process

--- src/app/components/auth/index.tsx
+++ src/app/components/auth/index.tsx
@@ -8,7 +8,7 @@ import styled from "styled-components";
 import LoginIcon from "@mui/icons-material/Login";
 import { T } from "../../../lib/types/common";
 import { Messages } from "../../../lib/config";
-import { MemberInput } from "../../../lib/types/member";
+import { LoginInput, MemberInput } from "../../../lib/types/member";
 import MemberService from "../../services/MemberService";
 import { sweetErrorHandling } from "../../../lib/sweetAlert";
 
@@ -64,12 +64,14 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
   const handlePasswordKeyDown = (e: T) => {
     if(e.key === "Enter" && signupOpen) {
       handleSignupRequest().then();
+    } else if(e.key === "Enter" && loginOpen) {
+      handleLoginRequest().then();
     }
   }
 
   const handleSignupRequest = async () => {
     try {
-      console.log("inputs:", memberNick, memberPhone, memberPasword)
+      // console.log("inputs:", memberNick, memberPhone, memberPasword)
       const isFulfill = memberNick !== "" && memberPhone !== "" && memberPasword !== "";
       if(!isFulfill) throw new Error(Messages.error3)
         
@@ -87,6 +89,28 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
     } catch (err) {
       console.log(err);
       handleSignupClose();
+      sweetErrorHandling(err).then();
+    }
+  }
+
+  const handleLoginRequest = async () => {
+    try {
+      const isFulfill = memberNick !== "" && memberPasword !== "";
+      if(!isFulfill) throw new Error(Messages.error3)
+        
+        const loginInput: LoginInput = {
+          memberNick: memberNick,
+          memberPassword: memberPasword,
+        };
+
+        const member = new MemberService();
+        const result = await member.login(loginInput);
+
+
+        handleLoginClose();
+    } catch (err) {
+      console.log(err);
+      handleLoginClose();
       sweetErrorHandling(err).then();
     }
   }
@@ -181,17 +205,21 @@ export default function AuthenticationModal(props: AuthenticationModalProps) {
                 label="username"
                 variant="outlined"
                 sx={{ my: "10px" }}
+                onChange={handleUsername}
               />
               <TextField
                 id={"outlined-basic"}
                 label={"password"}
                 variant={"outlined"}
                 type={"password"}
+                onChange={handlePasword}
+                onKeyDown={handlePasswordKeyDown}
               />
               <Fab
                 sx={{ marginTop: "27px", width: "120px" }}
                 variant={"extended"}
                 color={"primary"}
+                onClick={handleLoginRequest}
               >
                 <LoginIcon sx={{ mr: 1 }} />
                 Login
--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { serverApi } from "../../lib/config";
-import { Member, MemberInput } from "../../lib/types/member";
-import.meta.env.VITE_API_URL || "http://localhost:3003/";
+import { LoginInput, Member, MemberInput } from "../../lib/types/member";
 
 class MemberService {
   private readonly path: string;
@@ -54,6 +53,25 @@ class MemberService {
       throw err;
     }
   }
+
+    public async login(input: LoginInput): Promise<Member>{
+    try {
+        const url = this.path + "/member/login";
+        const result = await axios.post(url, input, {withCredentials: true});
+        console.log("login:", result);;
+        
+        
+        const member: Member = result.data.member;
+        console.log("member:", member);
+        localStorage.setItem("memberData", JSON.stringify(member));
+
+        return member;
+    } catch (err) {
+      console.log("Error, login:", err);
+      throw err;
+    }
+  }
+
 
 
 }
